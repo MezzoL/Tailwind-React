e91d8c1475788d131c0697ab74b7560fda12ae68
MezzoL <102746853+MezzoL@users.noreply.github.com>
2023-04-25 01:21:49 +0200

    main work

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div id = 'main'>
+            <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://upload.wikimedia.org/wikipedia/en/f/f3/SuguruGetolook.png" alt="" />
+            <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+                <div>
+                    <h1>I'm Mazen</h1>
+                    <h2>I'm a
+                        
+                    </h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
